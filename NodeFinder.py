e23b56c1314995beff5d89254195f8e07bbd4778
Jammicus <jamesmaxwalter@gmail.com>
2017-12-30 16:58:04 +0000

    Added method to print all offline nodes

--- NodeFinder.py
+++ NodeFinder.py
@@ -8,7 +8,16 @@ jenkins = Jenkins(JenkinsConfiguration.jenkinsurl)
 def printNodeNames():
     node_names = jenkins.get_nodes()
     for name in node_names.keys():
-        print (name)
+        print(name)
+
+def printOfflineNodes():
+    node_names = jenkins.get_nodes()
+    for item, value in node_names.iteritems():
+        if (value.is_online()==False):
+            print(item)
+
+
 
 if __name__ == "__main__":
-    printNodeNames()
+    # printNodeNames()
+    printOfflineNodes()
