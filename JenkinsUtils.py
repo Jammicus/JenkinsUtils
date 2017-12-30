1b955f5d281e28472b2de1b344c7ceb11e96952a
Jammicus <jamesmaxwalter@gmail.com>
2017-12-30 21:52:16 +0000

    Remove duplicate line, did some reformatting

--- JenkinsUtils.py
+++ JenkinsUtils.py
@@ -4,8 +4,8 @@ import JenkinsConfiguration
 
 from jenkinsapi.jenkins import Jenkins
 
+
 # https://jenkinsapi.readthedocs.io/en/latest/
-jenkins = Jenkins(JenkinsConfiguration.jenkinsurl)
 
 def printNodeNames():
     jenkins = Jenkins(JenkinsConfiguration.jenkinsurl)
@@ -13,24 +13,28 @@ def printNodeNames():
     for name in node_names.keys():
         print(name)
 
+
 def printOfflineNodes():
     jenkins = Jenkins(JenkinsConfiguration.jenkinsurl)
     node_names = jenkins.get_nodes()
     for item, value in node_names.iteritems():
-        if (value.is_online()==False):
+        if (value.is_online() == False):
             print(item)
+
 
 def printAllJobs():
     jenkins = Jenkins(JenkinsConfiguration.jenkinsurl)
     jobs = jenkins.get_jobs()
-    for key,value in jobs:
-        print (key)
+    for key, value in jobs:
+        print(key)
+
 
 def printInstalledPlugins():
-    jenkins = Jenkins(JenkinsConfiguration.jenkinsurl,JenkinsConfiguration.username,JenkinsConfiguration.password)
-    plugin_names = sorted(jenkins.get_plugins().keys(), key=lambda x:x.lower())
+    jenkins = Jenkins(JenkinsConfiguration.jenkinsurl, JenkinsConfiguration.username, JenkinsConfiguration.password)
+    plugin_names = sorted(jenkins.get_plugins().keys(), key=lambda x: x.lower())
     for item in plugin_names:
-        print (item)
+        print(item)
+
 
 p = argparse.ArgumentParser()
 subparsers = p.add_subparsers()
