a163d2649bc75f83ae612f3d1af9fc92db5d4803
Jammicus <jamesmaxwalter@gmail.com>
2017-12-30 17:32:07 +0000

    Clean up, allow command line arguments to trigger what you want

--- NodeFinder.py
+++ NodeFinder.py
@@ -1,29 +1,42 @@
-from jenkinsapi.jenkins import Jenkins
+import argparse
+import sys
 import JenkinsConfiguration
-import itertools
+
+from jenkinsapi.jenkins import Jenkins
 
 # https://jenkinsapi.readthedocs.io/en/latest/
 jenkins = Jenkins(JenkinsConfiguration.jenkinsurl)
 
-
-def printNodeNames():
+def printNodeNames(args):
     node_names = jenkins.get_nodes()
     for name in node_names.keys():
         print(name)
 
-def printOfflineNodes():
+def printOfflineNodes(args):
     node_names = jenkins.get_nodes()
     for item, value in node_names.iteritems():
         if (value.is_online()==False):
             print(item)
 
-def printAllJobs():
+def printAllJobs(args):
     jobs = jenkins.get_jobs()
     for key,value in jobs:
         print (key)
 
+p = argparse.ArgumentParser()
+subparsers = p.add_subparsers()
 
+option1_parser = subparsers.add_parser('nodeNames')
+option1_parser.set_defaults(func=printNodeNames)
+
+option2_parser = subparsers.add_parser('offlineNodes')
+option2_parser.set_defaults(func=printOfflineNodes)
+
+option3_parser = subparsers.add_parser('printAllJobs')
+# Add specific options for option3 here
+option3_parser.set_defaults(func=printAllJobs)
+args = p.parse_args()
+args.func(args)
 
 if __name__ == "__main__":
-    # printNodeNames()
-    printAllJobs()
+    option, params = sys.argv[0], sys.argv[1:]
